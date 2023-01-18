aba9e4e6ef2708001095a097a0081a83e768fb68
blitz <jeffersonvisser@gmail.com>
2023-01-18 10:52:02 +0100

    WAJOW

---
+++ js/data.js
@@ -0,0 +1,87 @@
+import file1 from '../gamedata/2.json' assert {type: 'json'}
+var gameinfo = file1;
+
+var gameinfo = file1;
+var gamename = gameinfo.data.name;
+var kortesamenvating = gameinfo.data.screenshots[0].path_thumbnail;
+console.log(`<img src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">"`)
+var link = "game.html";
+document.getElementById("titel1").innerHTML = `<a href="${link}">${gamename}</a>`;
+document.getElementById("img1").innerHTML = `<img class="gameimg" src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">`;
+
+import file2 from '../gamedata/3.json' assert {type: 'json'}
+var gameinfo = file2;
+
+var gameinfo = file2;
+var gamename = gameinfo.data.name;
+var kortesamenvating = gameinfo.data.screenshots[0].path_thumbnail;
+console.log(`<img src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">"`)
+var link = "game.html";
+document.getElementById("titel2").innerHTML = `<a href="${link}">${gamename}</a>`;
+document.getElementById("img2").innerHTML = `<img class="gameimg" src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">`;
+
+import file3 from '../gamedata/5.json' assert {type: 'json'}
+var gameinfo = file3;
+
+var gameinfo = file3;
+var gamename = gameinfo.data.name;
+var kortesamenvating = gameinfo.data.screenshots[0].path_thumbnail;
+console.log(`<img src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">"`)
+var link = "game.html";
+document.getElementById("titel3").innerHTML = `<a href="${link}">${gamename}</a>`;
+document.getElementById("img3").innerHTML = `<img class="gameimg" src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">`;
+
+import file4 from '../gamedata/10.json' assert {type: 'json'}
+var gameinfo = file4;
+
+var gameinfo = file4;
+var gamename = gameinfo.data.name;
+var kortesamenvating = gameinfo.data.screenshots[0].path_thumbnail;
+console.log(`<img src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">"`)
+var link = "game.html";
+document.getElementById("titel4").innerHTML = `<a href="${link}">${gamename}</a>`;
+document.getElementById("img4").innerHTML = `<img class="gameimg" src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">`;
+
+import file5 from '../gamedata/69.json' assert {type: 'json'}
+var gameinfo = file5;
+
+var gameinfo = file5;
+var gamename = gameinfo.data.name;
+var kortesamenvating = gameinfo.data.screenshots[0].path_thumbnail;
+console.log(`<img src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">"`)
+var link = "game.html";
+document.getElementById("titel5").innerHTML = `<a href="${link}">${gamename}</a>`;
+document.getElementById("img5").innerHTML = `<img class="gameimg" src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">`;
+
+import file6 from '../gamedata/212.json' assert {type: 'json'}
+var gameinfo = file6;
+
+var gameinfo = file6;
+var gamename = gameinfo.data.name;
+var kortesamenvating = gameinfo.data.screenshots[0].path_thumbnail;
+console.log(`<img src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">"`)
+var link = "game.html";
+document.getElementById("titel6").innerHTML = `<a href="${link}">${gamename}</a>`;
+document.getElementById("img6").innerHTML = `<img class="gameimg" src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">`;
+
+import file7 from '../gamedata/312.json' assert {type: 'json'}
+var gameinfo = file7;
+
+var gameinfo = file7;
+var gamename = gameinfo.data.name;
+var kortesamenvating = gameinfo.data.screenshots[0].path_thumbnail;
+console.log(`<img src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">"`)
+var link = "game.html";
+document.getElementById("titel7").innerHTML = `<a href="${link}">${gamename}</a>`;
+document.getElementById("img7").innerHTML = `<img class="gameimg" src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">`;
+
+import file8 from '../gamedata/666.json' assert {type: 'json'}
+var gameinfo = file8;
+
+var gameinfo = file8;
+var gamename = gameinfo.data.name;
+var kortesamenvating = gameinfo.data.screenshots[0].path_thumbnail;
+console.log(`<img src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">"`)
+var link = "game.html";
+document.getElementById("titel8").innerHTML = `<a href="${link}">${gamename}</a>`;
+document.getElementById("img8").innerHTML = `<img class="gameimg" src=\"${kortesamenvating}\" width=\"400px\" height=\"150px\">`;
